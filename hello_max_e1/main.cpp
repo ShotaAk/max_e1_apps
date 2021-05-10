9aa83c329002d620eafcf867447d0345117caf85
ShotaAk <macakasit@gmail.com>
2021-05-10 10:03:15 +0900

    Update hello_max_e1 to use max_e1.sensors

--- hello_max_e1/main.cpp
+++ hello_max_e1/main.cpp
@@ -1,6 +1,24 @@
-#include "max_e1_core.hpp"
+
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+#include "max_e1.hpp"
 
 int main() {
-    MaxE1Core max_e1("/dev/ttyACM0", 1000000);
-    max_e1.open_port();
+    MaxE1 max_e1("/dev/ttyACM0", 1000000);
+    if(max_e1.connect()){
+        int i = 0;
+        while(i < 100){
+            i++;
+
+            max_e1.sensors->update();
+            std::cout<<max_e1.sensors->get_temperature() << std::endl;
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+        
+
+    }
+    max_e1.disconnect();
 }
